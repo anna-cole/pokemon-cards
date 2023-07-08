bbe7c24fd5c8c4376674d454ac5667cbdd85bcb2
Anna Cole <acrrj123@gmail.com>
2023-07-08 08:06:53 -0500

    hide & seek for working

---
+++ README.md
@@ -0,0 +1,25 @@
+# Pokemon cards app
+
+## Description
+Welcome to our project! This is a Pokemon cards app made in Javascript. It allows users to quickly and easily see, create, delete, and update pokemon cards. With this app, users can effortlessly manage and organize their pokemon cards. We hope you enjoy and we look forward to your contributions!
+
+## Start up the server
+All of the pokemon data is stored in the db.json file. You'll access this data using a JSON server. Run json-server --watch db.json to start the server.
+
+## Contributing
+We welcome any and all contributions! Here are some ways you can get started:
+1. Report bugs: If you encounter any bugs, please let us know. Open up an issue and let us know the problem.
+2. Contribute code: If you are a developer and want to contribute, follow the instructions below to get started!
+3. Suggestions: If you don't want to code but have some awesome ideas, open up an issue explaining some updates or improvements you would like to see!
+4. Documentation: If you see the need for some additional documentation, feel free to add some!
+
+## Instructions
+1. Fork this repository
+2. Clone the forked repository
+3. Add your contributions (code or documentation)
+4. Commit and push
+5. Wait for pull request to be merged
+
+## Credits
+
+
--- index.js
+++ index.js
@@ -1,1 +1,91 @@
-document.addEventListener('DOMContentLoaded', () => console.log('We are connected'))
+// hide & seek with the form
+
+let addPokemon = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector("#new-pokemon-btn");
+  const pokemonFormContainer = document.querySelector(".container");
+  addBtn.addEventListener("click", () => {
+    addPokemon = !addPokemon;
+    if (addPokemon) {
+      pokemonFormContainer.style.display = "block";
+    } else {
+      pokemonFormContainer.style.display = "none";
+    }
+  });
+});
+
+// Fetch pokemons from the server. 
+
+// fetch('http://localhost:3000/pokemons')
+// .then(resp => resp.json())
+// .then(data => data.forEach(pokemon => renderPokemons(pokemon)))
+
+// // With the response data, render the pokemons in the DOM, by making a card <div class="card"> for each pokemon and adding it to the pokemon-collection div. Then, add pokemon info into the cards.
+
+// function renderPokemons(pokemon) {
+//     let pokemonContainer = document.querySelector('#pokemon-collection');
+//     let card = document.createElement('div');
+//     card.className = 'card';
+
+//     card.innerHTML = `
+//     <h2>${pokemon.name}</h2>
+//     <img src="${pokemon.image}" class="pokemon-avatar">
+//     <p>${pokemon.likes} likes</p>
+//     <button class="like-btn" id=${pokemon.id}>Like ❤️</button>`
+    
+//     const likeBtn = card.querySelector('.like-btn')
+//     likeBtn.addEventListener('click', () => {
+//       let p = card.querySelector('p')
+//       p.textContent = `${pokemon.likes+= 1} likes`
+//       updateLikes(pokemon.id, pokemon.likes)
+//     })
+//     pokemonContainer.appendChild(card)
+//   }
+
+// // Add a new pokemon. When a user submits the pokemon form, send a POST request to add the new pokemon to the collection. If the post is successful, the pokemon should be added to the DOM without reloading the page.
+
+// const form = document.querySelector('.add-pokemon-form')
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   //console.log(e.target.name)
+//   let newPokemonObj = {
+//     name: e.target.name.value,
+//     image: e.target.image.value,
+//     likes: 0
+//   }
+//   renderNewPokemon(newPokemonObj)
+//   e.target.reset()
+// })
+
+// // Then, make the POST request to send to the server the new card.
+// function renderNewPokemon(newPokemonObj) {
+//   const configObj = {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Accept: 'application/json'
+//     },
+//     body: JSON.stringify(newPokemonObj)
+//   }
+//   fetch('http://localhost:3000/pokemons', configObj)
+//   .then(res => res.json())
+//   .then(newPokemon => renderPokemons(newPokemon))
+// }
+
+// // Then, make the PATCH request to update the number of likes in the server.
+// function updateLikes(id, newNumberOfLikes) {
+//   fetch(`http://localhost:3000/pokemons/${id}`, {
+//     method: 'PATCH',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Accept: 'application/json'
+//     },
+//     body: JSON.stringify({likes: newNumberOfLikes})
+//   })
+//   .then(resp => resp.json())
+//   .then(updatedPokemon => console.log(updatedPokemon))
+// }
+
+
